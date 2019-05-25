040c8356ca36fbb83a228af8b4094db685e8baaa
Sunjian584970824 <584970824@qq.com>
2019-05-25 17:35:07 +0800

    nodejs+图片上传+vue前端代码

--- server/router.js
+++ server/router.js
@@ -2,18 +2,41 @@
 const models = require('./db');
 const express = require('express');
 const router = express.Router();
+const Koa = require('koa')
+const logger = require('koa-logger')
+const formidable = require('formidable')
+const koaBody = require('koa-body')
+const util = require('util')
+const path = require('path')
+const staticCache = require('koa-static-cache')
+const fs = require('fs')
+var readDir = fs.readdirSync("./img");
+// console.log(readDir);
+// readDir.map(item=>{
+//   console.log(item)
 
 /************** 创建(create) 读取(get) 更新(update) 删除(delete) **************/
-
+// fs.readFile('./img/'+item,function(err, data){
+//   if(err) console.log('写文件操作失败',err);
+//   else{
+//     console.log('写文件操作成功',data);
+//     fs.writeFile('./img/teset.jpg', data,function(errs){
+//       if(errs) console.log('写文件操作失败');
+//       else console.log('写文件操作成功');
+//     });
+//   }
+// });
+// })
 // 创建账号接口
-router.post('/api',(req,res) => {
+router.post('/api', (req, res) => {
+  console.log(req.body)
   // 这里的req.body能够使用就在index.js中引入了const bodyParser = require('body-parser')
   let newAccount = new models.Login({
-    account : req.body.account,
-    password : req.body.password
+    account: req.body.account,
+    password: req.body.password
   });
   // 保存数据newAccount数据进mongoDB
-  newAccount.save((err,data) => {
+  newAccount.save((err, data) => {
     if (err) {
       res.send(err);
     } else {
@@ -21,16 +44,29 @@ router.post('/api',(req,res) => {
     }
   });
 });
-// 获取已有账号接口
-router.get('/api/login/getAccount',(req,res) => {
-  // 通过模型去查找数据库
-  models.Login.find((err,data) => {
-    if (err) {
-      res.send(err);
-    } else {
-      res.send(data);
+// 图片上传
+router.post('/img', (req, res) => {
+  const form = new formidable.IncomingForm()
+  form.encoding = 'utf-8';
+  form.uploadDir = path.join(__dirname + "/img");
+  form.keepExtensions = true;//保留后缀
+  form.maxFieldsSize = 2 * 1024 * 1024;
+  // form.uploadDir = "./uploads";
+  form.parse(req, function (err, frelds, files) {
+    var filename =files.tweetPic.name
+    var nameArray = filename.split('.');
+    var type = nameArray[nameArray.length - 1];
+    var name = '';
+    for (var i = 0; i < nameArray.length - 1; i++) {
+      name = name + nameArray[i];
     }
-  });
-});
+    var date = new Date();
+    var time = '_' + date.getFullYear() + "_" + date.getMonth() + "_" + date.getDay() + "_" + date.getHours() + "_" + date.getMinutes();
+    var avatarName = name + time + '.' + type;
+    var newPath = form.uploadDir + "/" + avatarName;
 
+    fs.renameSync(files.tweetPic.path, newPath);  //重命名
+    res.send({data:"/upload/"+avatarName,files:files})
+  })
+})
 module.exports = router;
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,113 +1,78 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+  <div>
+
+    <el-form :inline="true" :model="formInline" class="demo-form-inline">
+      <el-form-item label="审批人">
+        <el-input v-model="formInline.user" placeholder="审批人"></el-input>
+      </el-form-item>
+      <el-form-item label="审批人">
+        <el-input v-model="formInline.region" placeholder="审批人"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit">查询</el-button>
+      </el-form-item>
+      <el-form-item label="上传图片">
+        <el-input v-model="formInline.file" id="file" type="file" placeholder="审批人"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="upTest">上传</el-button>
+      </el-form-item>
+    </el-form>
+    <img v-if="src" :src="src" alt="">
   </div>
 </template>
-
 <script>
-export default {
-  name: 'HelloWorld',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+  export default {
+    data() {
+      return {
+        src:'',
+        formInline: {
+          user: '',
+          region: '',
+          file: ''
+        }
+      }
+    },
+    methods: {
+      upTest(e) {
+        var el = document.getElementById('file').files[0]
+        var url= this.getObjectURL(el)
+        this.src=url
+        var param = new FormData(); //创建form对象
+        param.append('tweetPic',el,el.name);//通过append向form对象添加数据
+        //param.append('chunk','0');//添加form表单中其他数据
+        //console.log(param.get('tweetPic')); //FormData私有类对象，访问不到，可以通过get判断值是否传进去
+        let config = {
+          headers:{'Content-Type':'multipart/form-data'}
+        };  //添加请求头
+        this.$axios.post('img',param,config)
+          .then(response=>{
+            console.log(response.data);
+          })
+      },
+      getObjectURL(file) {
+        var url = null;
+        if (window.createObjectURL != undefined) {
+          url = window.createObjectURL(file);
+        } else if (window.URL != undefined) {
+          url = window.URL.createObjectURL(file);
+        } else if (window.webkitURL != undefined) {
+          url = window.webkitURL.createObjectURL(file);
+        }
+        return url;
+      },
+      onSubmit() {
+        this.$axios({
+          method: 'post',
+          url: 'api',
+          data: {
+            account: this.formInline.user,
+            password: this.formInline.region
+          }
+        }).then(res => {
+          console.log(res)
+        });
+      }
     }
   }
-}
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
